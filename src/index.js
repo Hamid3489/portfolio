7c4f8db0ad4e483aef9a686b96adf7a3c5fa292d
Hamid3489 <hamid.f3489@gmail.com>
2023-10-02 01:25:56 +0500

    nedf

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+ReactDOM.render(
+  <Router basename="/porfolio">
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
